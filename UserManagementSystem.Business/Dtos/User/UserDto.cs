24cf7149d430ad3ef7adfb02a66bba2a479daaa8
HTarık <244035008@ogr.cbu.edu.tr>
2025-04-27 16:32:28 +0300

    refactor: Remove unused fields from DTOs

--- UserManagementSystem.Business/Dtos/User/UserDto.cs
+++ UserManagementSystem.Business/Dtos/User/UserDto.cs
@@ -2,9 +2,11 @@ namespace UserManagementSystem.Business.Dtos;
 
 public class UserDto
 {
-    public int  UserId { get; set; }
+    public int UserId { get; set; }
     public string Username { get; set; } = null!;
     public string Email { get; set; } = null!;
+    
+    public string PhoneNumber { get; set; } = null!;
     
     public string Department { get; set; } = null!;
     public List<RoleDto> Roles { get; set; } = null!;
--- UserManagementSystem.Business/Dtos/User/UserUpdateDto.cs
+++ UserManagementSystem.Business/Dtos/User/UserUpdateDto.cs
@@ -2,7 +2,6 @@ namespace UserManagementSystem.Business.Dtos;
 
 public class UserUpdateDto
 {
-    public int UserId { get; set; }
     public string UserName { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Phone { get; set; } = null!;
