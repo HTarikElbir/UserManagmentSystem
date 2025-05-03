3c01e1bd8aadc9ea35de167a1df9f34d473bdb75
HTarık <244035008@ogr.cbu.edu.tr>
2025-05-03 19:13:31 +0300

    feat: Add RoleDto -> Role Mapping

--- UserManagementSystem.Business/MappingProfiles/RoleProfile.cs
+++ UserManagementSystem.Business/MappingProfiles/RoleProfile.cs
@@ -13,5 +13,9 @@ public class RoleProfile : Profile
             .ForMember(dest => dest.RoleId, opt => opt.MapFrom(src => src.RoleId))
             .ForMember(dest => dest.RoleName, opt => opt.MapFrom(src => src.RoleName));
         
+        // RoleDto -> Role
+        CreateMap<RoleDto, Role>()
+            .ForMember(dest => dest.RoleId, opt => opt.MapFrom(src => src.RoleId))
+            .ForMember(dest => dest.RoleName, opt => opt.MapFrom(src => src.RoleName));
     }
 }
