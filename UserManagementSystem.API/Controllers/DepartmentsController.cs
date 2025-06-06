d88a30f3e48239066d2493cc25bd1fa13c54a80e
HTarık <244035008@ogr.cbu.edu.tr>
2025-06-07 02:25:46 +0300

    feat: Add GetDepartmentByNameAsync endpoint to DepartmentsController

--- UserManagementSystem.API/Controllers/DepartmentsController.cs
+++ UserManagementSystem.API/Controllers/DepartmentsController.cs
@@ -15,18 +15,29 @@ public class DepartmentsController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<IActionResult> GetAllDepartments(int page = 1, int pageSize = 10)
+    public async Task<IActionResult> GetAllDepartmentsAsync(int page = 1, int pageSize = 10)
     {
         return Ok(await _departmentService.GetAllAsync(page, pageSize));
     }
 
     [HttpGet("by-id/{id}")]
-    public async Task<IActionResult> GetDepartmentById(int id)
+    public async Task<IActionResult> GetDepartmentByIdAsync(int id)
     {
         if (id <= 0)
             return BadRequest("Invalid Department Id");
 
         var result = await _departmentService.GetByIdAsync(id);
+        
+        return Ok(result);
+    }
+
+    [HttpGet("by-name/{name}")]
+    public async Task<IActionResult> GetDepartmentByNameAsync(string name)
+    {
+        if(string.IsNullOrEmpty(name))
+            return BadRequest("Invalid Department Name");
+        
+        var result = await _departmentService.GetByNameAsync(name);
         
         return Ok(result);
     }
