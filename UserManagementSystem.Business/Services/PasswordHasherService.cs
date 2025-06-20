2320a89f1f17e33f14efe58c3303e1d455a84930
HTarık <244035008@ogr.cbu.edu.tr>
2025-06-20 19:38:44 +0300

    feat: Add log logic to PasswordHasherService

--- UserManagementSystem.Business/Services/PasswordHasherService.cs
+++ UserManagementSystem.Business/Services/PasswordHasherService.cs
@@ -1,18 +1,42 @@
+using Microsoft.Extensions.Logging;
 using UserManagementSystem.Business.Interfaces;
 
 namespace UserManagementSystem.Business.Services;
 
 public class PasswordHasherService : IPasswordHasher
 {
+    private readonly ILogger<PasswordHasherService> _logger;
+    
+    public PasswordHasherService(ILogger<PasswordHasherService> logger)
+    {
+        _logger = logger;
+    }
+    
     // Used to hash the password
     public string HashPassword(string password)
     {
-        return BCrypt.Net.BCrypt.HashPassword(password);
+        try
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Password hashing failed");
+            throw;
+        }
     }
 
     // Used to verify the hashed password
     public bool VerifyPassword(string password, string hashedPassword)
     {
-        return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        try
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Password verification failed");
+            throw;
+        }
     }
 }
